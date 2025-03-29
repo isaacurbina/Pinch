6f96b792a6512272f09baa497a83f2c0eb0588ed
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-29 16:26:26 -0500

    first commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Isaac Urbina on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
