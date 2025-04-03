7c67c5ed1adfbf6ed0eb83d65bb05bcf9c91978d
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-03 12:31:36 -0500

    77. 5. SwiftUI Control Interface

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -93,6 +93,58 @@ struct ContentView: View {
 				, alignment: .top
 			)
 			
+			
+			// MARK: - controls
+			.overlay(
+				Group {
+					HStack {
+						// scale down
+						Button {
+							withAnimation(.spring()) {
+								if imageScale > 1 {
+									imageScale -= 1
+									
+									if imageScale <= 1 {
+										resetImageState()
+									}
+								}
+							}
+						} label: {
+							ControlImageView(icon: "minus.magnifyingglass")
+						}
+						
+						// reset
+						Button {
+							resetImageState()
+						} label: {
+							ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+						}
+						
+						// scale up
+						Button {
+							withAnimation(.spring()) {
+								if imageScale < 5 {
+									imageScale += 1
+									
+									if imageScale > 5 {
+										imageScale = 5
+									}
+								}
+							}
+						} label: {
+							ControlImageView(icon: "plus.magnifyingglass")
+						}
+						
+					} // HStack
+					.padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+					.background(.ultraThinMaterial)
+					.cornerRadius(12)
+					.opacity(isAnimating ? 1 : 0)
+				}
+					.padding(.bottom, 30)
+				, alignment: .bottom
+			)
+			
 		} // NavigationView
 		.navigationViewStyle(.stack)
     }
