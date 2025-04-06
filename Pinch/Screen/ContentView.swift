b3047f01e87868fde570f4ccb23ef2fbf9fb1fe5
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-05 23:34:41 -0500

    78. 6. SwiftUI Magnification Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -75,6 +75,29 @@ struct ContentView: View {
 							}
 						)
 				
+				
+				// MARK: - magnification
+				
+					.gesture(
+						MagnificationGesture()
+							.onChanged { value in
+								withAnimation(.linear(duration: 1)) {
+									if imageScale >= 1 && imageScale <= 5 {
+										imageScale = value
+									} else if imageScale > 5 {
+										imageScale = 5
+									}
+								}
+							}
+							.onEnded { _ in
+								if imageScale > 5 {
+									imageScale = 5
+								} else if imageScale <= 1 {
+									resetImageState()
+								}
+							}
+						)
+				
 			} // Stack
 			.navigationTitle("Pinch & Zoom")
 			.navigationBarTitleDisplayMode(.inline)
